69179a338642e1cd04b863867bb4203787cd84b2
Vladimir <vvsergeev87@gmail.com>
2023-01-22 19:59:18 +0300

    add index.html,

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
 
 # Create your views here.
+file_path = '/home/student/Projects/DjangoCountries/country-by-languages.json'
+file = open(file_path, 'r')
+string_list = file.readlines()
+print(string_list)
+
+file.close()
+
+def main_page(request) -> HttpResponse:
+    return render(request, 'index.html')
+

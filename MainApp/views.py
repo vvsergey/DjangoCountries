bd8149c1d2d1dd06d604851259d6d42789255021
Vladimir <vvsergeev87@gmail.com>
2023-01-27 19:03:46 +0300

    add countires dict

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,16 +3,25 @@ from django.http import HttpResponseNotFound
 
 # Create your views here.
 file_path = '/home/student/Projects/DjangoCountries/country-by-languages.json'
+countries = {}
 with open(file_path, 'r') as in_file:
     for line in in_file.readlines():
-        print(line)
+        ln = line.strip()
+        if ln.find('country') > -1:
+            country = ln[12:-2]
+            countries[country] = []
+        if len(ln) > 2 and ln.find('languages') == -1 and ln.find('country') == -1:
+            countries[country].append(ln.strip('",'))
 
 def main_page(request) -> HttpResponse:
     return render(request, 'index.html')
 
 
 def countries_list(request) -> HttpResponse:
-    return render(request, 'countries-list.html')
+    context = {
+        "countries": countries
+    }
+    return render(request, 'countries-list.html', context)
 
 
 def country_info(request, country: str) -> HttpResponse:

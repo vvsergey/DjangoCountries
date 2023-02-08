9f3181118e953189ef412d548215455c48ac3f44
Vladimir <vvsergeev87@gmail.com>
2023-02-08 19:58:03 +0300

    load data  to BD

--- MainApp/add_data.py
+++ MainApp/add_data.py
@@ -1,10 +1,20 @@
 import json
 from MainApp.models import Countries
 
-
 file_path = '/home/student/Projects/DjangoCountries/country-by-languages.json'
 with open(file_path, 'r') as in_file:
-    for line in in_file.readlines():
-        print(line)
-    # data = json.load(in_file)
+    data = json.load(in_file)
+
+for el in data:
+    cnt = el['country']
+    lang = ','.join(el['languages'])
+    cntr = Countries(country=cnt, languages=lang)
+    #cntr.save()
+
+
+
+
+
+
+
 
--- MainApp/models.py
+++ MainApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Countries(models.Model):
     country = models.CharField(max_length=50)
     languages = models.CharField(max_length=500)
